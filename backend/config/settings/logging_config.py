dee7ea7809e1b60d40695b03b73c1531678ac51c
Philippe Ducasse <ducassephi@hotmail.fr>
2026-09-24 11:22:03 +0200

    fix: added log env

--- backend/config/settings/logging_config.py
+++ backend/config/settings/logging_config.py
@@ -1,40 +1,50 @@
 import os
 
 LOGLEVEL = os.getenv("DJANGO_LOGLEVEL", "debug").upper()
-LOG_DIR = os.getenv("LOG_DIR", "/var/log/zirkusmond")
+LOG_DIR = os.getenv("LOG_DIR", None)
+
+# Build handlers list - always include console, add file only if LOG_DIR is set
+handlers_list = ["console"]
+handlers_config = {
+    "console": {
+        "class": "logging.StreamHandler",
+        "formatter": "console",
+    },
+}
+
+formatters_config = {
+    "console": {
+        "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s",
+    },
+}
+
+# Only enable file logging if LOG_DIR is explicitly set (production)
+if LOG_DIR:
+    handlers_list.append("file")
+    handlers_config["file"] = {
+        "class": "logging.handlers.RotatingFileHandler",
+        "filename": os.path.join(LOG_DIR, "django.log"),
+        "maxBytes": 100 * 1024 * 1024,  # 100MB
+        "backupCount": 10,
+        "formatter": "file",
+    }
+    formatters_config["file"] = {
+        "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+    }
 
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "formatters": {
-        "console": {
-            "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s",
-        },
-        "file": {
-            "format": "%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-        },
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "console",
-        },
-        "file": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": os.path.join(LOG_DIR, "django.log"),
-            "maxBytes": 100 * 1024 * 1024,  # 100MB
-            "backupCount": 10,
-            "formatter": "file",
-        },
-    },
+    "formatters": formatters_config,
+    "handlers": handlers_config,
     "loggers": {
         "": {
             "level": LOGLEVEL,
-            "handlers": ["console", "file"],
+            "handlers": handlers_list,
         },
         "django.db.backends": {
             "level": "WARNING",
-            "handlers": ["console", "file"],
+            "handlers": handlers_list,
             "propagate": False,
         },
         "django.utils.autoreload": {
